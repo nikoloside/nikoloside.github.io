32954f52e7c3b2286d3f0b72866df760068faac3
nikoloside <nikoloside@gmail.com>
2025-06-28 03:10:55 +0900

    Add Google Analytics tracking and improve .gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import { AnimatePresence } from 'framer-motion'
+import { useEffect } from 'react'
 import Navbar from './Navbar'
 import MainPage from './pages/MainPage'
 import Demos from './pages/Demos'
 import CocTrpg from './pages/CocTrpg'
 import './App.css'
 
+// Google Analytics page tracking component
+function PageTracker() {
+  const location = useLocation()
+  
+  useEffect(() => {
+    if (typeof window.gtag !== 'undefined') {
+      window.gtag('config', 'G-8FJ3FS3HFB', {
+        page_path: location.pathname + location.search
+      })
+    }
+  }, [location])
+  
+  return null
+}
+
 function App() {
   return (
     <Router>
+      <PageTracker />
       <div className="app">
         <Navbar />
         <AnimatePresence mode="wait">
